b728d37c08eced2bdba51bc480ccfbc99fa728ef
jeminsieow <jemin.sieow@prepr.org>
2020-12-06 16:37:29 +0800

    Refactor context to separate file

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import createDataContext from './createDataContext'
 
 const BlogContext = React.createContext();
 
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_blogpost':
+      return [...state, { title: `Blog Post #${state.length + 1}` }];
+    default:
+      return state;
+  }
+};
+
 export const BlogProvider = ({ children }) => {
-  return <BlogContext.Provider>
+  const [blogPosts, dispatch] = useReducer(blogReducer, []);
+
+  const addBlogPost = () => {
+    dispatch({ type: 'add_blogpost' });
+  };
+
+  return <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
     {children}
   </BlogContext.Provider>
-}
+};
+
+export default BlogContext;

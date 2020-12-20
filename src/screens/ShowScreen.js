5818f62e285bb097dfe504e99d6f2925a2983cd6
jeminsieow <jemin.sieow@prepr.org>
2020-12-21 00:16:52 +0800

    Add Edit function

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Context } from '../context/BlogContext';
+import { FontAwesome } from '@expo/vector-icons';
 
 const ShowScreen = ({ navigation }) => {
   const { state } = useContext(Context);
@@ -9,7 +11,21 @@ const ShowScreen = ({ navigation }) => {
 
   return <>
     <Text>{ blogPost.title }</Text>
+    <Text>{ blogPost.content }</Text>
   </>
+}
+
+ShowScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity onPress={
+        () => navigation.navigate('Edit', {id: navigation.getParam('id')})
+      }
+      >
+        <FontAwesome name="pencil" size={30} />
+      </TouchableOpacity>
+    ),
+  };
 }
 
 const styles = StyleSheet.create({

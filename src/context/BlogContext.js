bb753cadbfaea8eb088af3fc06fc796d734710ff
jeminsieow <jemin.sieow@prepr.org>
2020-12-20 18:18:17 +0800

    Add Create and Show screens

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -5,7 +5,8 @@ const blogReducer = (state, action) => {
     case 'add_blogpost':
       return [...state, { 
         id: Math.floor(Math.random() * 99999), 
-        title: `Blog Post #${state.length + 1}` 
+        title: action.payload.title,
+        content: action.payload.content
       }];
     case 'delete_blogpost':
       return state.filter((blogPost) => blogPost.id !== action.payload);
@@ -15,8 +16,9 @@ const blogReducer = (state, action) => {
 };
 
 const addBlogPost = (dispatch) => {
-  return () => {
-    dispatch({ type: 'add_blogpost' });
+  return (title, content, callback) => {
+    dispatch({ type: 'add_blogpost', payload: { title, content } });
+    callback();
   };
 };
 
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,27 +3,40 @@ import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'reac
 import { Context as BlogContext } from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons'
 
-const IndexScreen = () => {
-  const { state, addBlogPost, deleteBlogPost } = useContext(BlogContext);
+const IndexScreen = ({ navigation }) => {
+  const { state, deleteBlogPost } = useContext(BlogContext);
 
   return (
     <>
-      <Button title="Add Post" onPress={addBlogPost}/>
       <FlatList 
         data={state}
         keyExtractor={(blogPost) => blogPost.title}
         renderItem={({ item }) => {
-          return <View style={styles.row}>
-            <Text style={styles.title}>{item.title} - {item.id}</Text>
-            <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
-              <Feather style={styles.icon} name="trash" />
+          return (
+            <TouchableOpacity onPress={() => navigation.navigate('Show', { id: item.id })}>
+              <View style={styles.row}>
+                <Text style={styles.title}>{item.title} - {item.id}</Text>
+                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                  <Feather style={styles.icon} name="trash" />
+                </TouchableOpacity>
+              </View>
             </TouchableOpacity>
-          </View>
+          )
         }}
       />
     </>
   )
 };
+
+IndexScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity onPress={() => navigation.navigate('Create')}>
+        <Feather name="plus" size={30} />
+      </TouchableOpacity>
+    ),
+  };
+}
 
 const styles = StyleSheet.create({
   row: {
